2a0fae7894df5235886590321301a107f5537a85
Oaní da Silva da Costa <oani_costa@outlook.com.br>
2026-02-27 21:16:27 -0300

    test cors

--- app.js
+++ app.js
@@ -7,9 +7,9 @@ const app = express();
 
 app.use(express.json());
 
-app.use(cors({
-origin: '*'
-}));
+// app.use(cors({
+// origin: '*'
+// }));
 
 app.use(require('./routes/userRoutes'));
 app.use(require('./routes/testRoutes'));
